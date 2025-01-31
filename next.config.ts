628b5c05a13ac0570da76939100fcc709b971ad9
simalkangalli <simal.kangalli@gmail.com>
2025-01-31 18:45:40 +0300

    second commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["cdn.dummyjson.com"], // Harici görsel kaynağını ekledik
+  },
 };
+  
+  
+
 
 export default nextConfig;
