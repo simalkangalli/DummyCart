687098f586ce4ae8a241535ee8c1dabd5719e634
simalkangalli <simal.kangalli@gmail.com>
2025-01-31 19:23:07 +0300

    commiitt

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["cdn.dummyjson.com"], // Harici görsel kaynağını ekledik
+    domains: ["cdn.dummyjson.com"], // api için cdn eklendi 
   },
 };
   
--- src/app/components/api/route.ts
+++ src/app/components/api/route.ts
@@ -3,3 +3,8 @@ import { NextResponse } from "next/server";
 export async function GET() {
   return NextResponse.json({ message: "API Çalışıyor!" });
 }
+export async function POST(request: Request) {
+    const body = await request.json();
+    return NextResponse.json({ data: body });
+  }
+  
